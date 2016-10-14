523902ea13636cc5103c09f4337537cbf39a1409
dorgon <dorgonman@hotmail.com>
2016-10-14 18:21:48 +0800

    implementing HorizonUE4Build

---
+++ HorizonBuildTool/Source/HorizonUE4Build/HorizonUE4Build.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*- 
+import sys
+from optparse import OptionParser;
+from xml.dom import minidom;
+
+
+import re
+import os
+import csv
+import hashlib
+import shutil
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from HorizonBuildFileUtil import HorizonBuildFileUtil
+import subprocess
+
+
+class HorizonUE4Build(object):
+    """description of class"""
+    def __init__(self):
+         #current tool version is 1
+        self.m_iCodeVersion = 1
+        self.m_sConfig = "default";
+        self.m_sOutReportFilePath = "Output/HorizonUE4BuildReport.log"
+        self.m_sClean = False
+
+    def __generateOptionParser__(self):
+        parser = OptionParser();
+        parser.add_option("--config", dest="config",
+                      default="./Config/HorizonUE4Build/UE4Build_sample.xml",
+                      help="config file", metavar="FILE")
+        parser.add_option("--clean", action="store_true", dest="clean")
+
+
+        parser.add_option("--engine", dest="unreal_engine_root",
+                      default="UnrealEngineRoot",
+                      help="root path of unreal engine", metavar="FILE")
+
+
+        parser.add_option("--project", dest="project_file_full_path",
+                      default="project_file_full_path",
+                      help="project_file_full_path", metavar="project_file_full_path")
+
+
+        parser.add_option("--build_platform", dest="build_platform",
+                      default="win64",
+                      help="ex: Win64, Win32, Android...", metavar="build_platform")
+
+        parser.add_option("--build_config", dest="build_config",
+                      default="win64",
+                      help="ex: Win64, Win32, Android...", metavar="build_config")
+        parser.add_option("--archive", dest="build_archive_path",
+                      default="./Archive/Build/",
+                      help="build_archive_path", metavar="build_archive_path")
+        return parser;
+    def init(self):
+        print("curretn folder:" + os.getcwd() + "\n")
+        parser = self.__generateOptionParser__()
+        (options, args) = parser.parse_args()
+        print("options:" + str(options))
+        print("args" + str(args))
+        if(options.config != None):
+           self.m_sConfig = options.config;
+
+        if(options.clean != None):
+           self.m_sClean = options.clean;
+
+        if(options.unreal_engine_root != None):
+           self.m_sUnrealEngineRoot = options.unreal_engine_root;
+
+        if(options.project_file_full_path != None):
+           self.m_sProjectFileFullPath = options.project_file_full_path;
+
+
+        if(options.build_platform != None):
+           self.m_sBuildPlatform = options.build_platform;
+
+        if(options.build_archive_path != None):
+           self.m_sBuildArchivePath = options.build_archive_path;
+
+
+        print("m_sUnrealEngineRoot:" + str(self.m_sUnrealEngineRoot))
+        print("m_sProjectFileFullPath:" + str(self.m_sProjectFileFullPath))
+        print("m_sBuildPlatform:" + str(self.m_sBuildPlatform)) 
+        print("m_sBuildArchivePath:" + str(self.m_sBuildArchivePath))
+        #xmldoc = minidom.parse(self.m_sConfig)   
+        #self.m_sHorizonEngineRoot = os.path.abspath(xmldoc.getElementsByTagName('UnrealEngineRoot')[0].firstChild.nodeValue);
+
+
+
+    def execute(self): 
+        HorizonBuildFileUtil.HorizonBuildFileUtil.EnsureDir(self.m_sOutReportFilePath)
+        reportFile = open(self.m_sOutReportFilePath, 'w', encoding = 'utf-8')
+        reportFile.truncate()
+        reportFile.close()
+        if(self.m_sClean == True):
+            print("start clean project")
+            try:
+             pass
+            except:
+                pass 
+        else:
+            print("start build project")
+           
---
+++ HorizonBuildTool/Source/HorizonUE4Build/Main.py
@@ -0,0 +1,13 @@
+from HorizonUE4Build import HorizonUE4Build
+
+def main():
+    app = HorizonUE4Build()
+    app.init();
+    app.execute();
+
+
+
+
+
+if __name__ == '__main__':
+    main();    
